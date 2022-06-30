5a81f1f50cbd24da94a8ddd1d54f8f961322ba1c
Abrish-Green <abrham365muche@gmail.com>
2022-06-30 11:12:02 +0300

    Added Cors

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const dotenv = require('dotenv').config()
 const API = express.Router()
 const port = process.env.PORT
 const connectDB = require('./config/mongodb')
+var cors = require('cors')
 
 const item = require('./route/item')
 app.use(express.json());
+app.use(cors())
 
 app.use((req,res,next)=>{
   console.log(req.url)
